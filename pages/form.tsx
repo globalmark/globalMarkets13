c4207e6e13e54621d63bdd00966c74d5c5d1c653
emaromerot <86328104+emaromerot@users.noreply.github.com>
2022-06-07 23:29:38 -0300

    Create form.tsx

---
+++ pages/form.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next"
+import Link from "next/link"
+
+const form: NextPage= () =>{
+    return ( 
+        <div>
+            <div>
+                <h2> En esta seccion podra añadir a su perfil un nuevo producto para la venta </h2>
+            </div>
+            <div>
+                <Link href="/home">
+                     <button> Home </button>
+                    </Link>
+            </div>
+            <div>
+                <label > Nombre:
+                 </label>
+                 <input type="text" />
+            </div>
+            <div>
+                <label>
+                    select
+                </label>
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default form
