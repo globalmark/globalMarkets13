c323fa47375d664eb87b220cc4a0c1401ccae2b8
yanfranve <franklinyanez6198@gmail.com>
2022-06-09 06:12:09 -0500

    atlizazion de navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import {
 } from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layouts";
-import { initialData } from "../database/products";
+//import { initialData } from "../database/products";
 
 import Image from "next/image";
 
@@ -22,21 +22,6 @@ const Home: NextPage = () => {
       <Typography variant="h2" sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
-      <Grid container spacing={4}>
-        {initialData.products.map((product) => (
-          <Grid iten xs={6} sm={4} key={product.slug}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  componect="img"
-                  Image={`products/${product.images[0]}`}
-                  alt={product.title}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
     </ShopLayout>
   );
 };
