9088eeb373fff6ce24538c2030c06298d1c92391
yanfranve <franklinyanez6198@gmail.com>
2022-06-13 16:36:38 -0500

    actulizacion front

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,27 @@
-import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import type { NextPage } from "next";
+import { Typography } from "@mui/material";
 
-import { ShopLayout } from '../components/layouts';
+import { ShopLayout } from "../components/layouts";
 
-import { ProductList } from '../components/products';
-import { useProducts } from '../hooks';
+import { ProductList } from "../components/products";
+import { useProducts } from "../hooks";
 
-import { FullScreenLoading } from '../components/ui';
+import { FullScreenLoading } from "../components/ui";
 
 const HomePage: NextPage = () => {
-
-    const { products, isLoading } = useProducts("/products");
-return (
-    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
-        <Typography variant='h1' component='h1'>Tienda</Typography>
-        <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
-        {
-            isLoading
-                ? <FullScreenLoading />
-                : <ProductList products={ products } />
-        }
+  const { products, isLoading } = useProducts("/products");
+  return (
+    <ShopLayout
+      title={"Global-Market - Home"}
+      pageDescription={"Encuentra los mejores productos aqui en Global Market"}>
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+      {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
     </ShopLayout>
-    )
-}
-export default HomePage
+  );
+};
+export default HomePage;
