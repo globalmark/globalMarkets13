db61cbd41498b737edde2904e6124e7027177e36
dperco <danielperco4@gmail.com>
2022-06-23 18:27:45 -0300

    el login con Google

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -12,19 +12,11 @@ const routerProducts = require('./routesProducts');
 // create user
 router.use('/users/',routerUser);
 
-//const  routeGoogles  = require('../routes/routesGoogle.js');
-//middleware
 
 router.use('/auth/', userRouter);
 router.use('/paypal/', paypalrouter);
 
-// router.use('/authGoogle',passport.authenticate('auth-google',{
-//     scope:[
-//         "https://www.googleapis.com/auth/userinfo.profile",
-//         "https://www.googleapis.com/auth/userinfo.email"
-//        ],
-//        session:false 
-// }),routeGoogles); 
+
 
 
 router.use('/products/', routerProducts);
