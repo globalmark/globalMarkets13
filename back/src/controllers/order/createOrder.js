9f336e23e64340c5f01b817bd213b1727c14d191
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-06-23 01:08:43 -0500

    order-funcional

--- back/src/controllers/order/createOrder.js
+++ back/src/controllers/order/createOrder.js
@@ -1,17 +1,58 @@
-const orderSchema = require('../../models/order');
-const createOrder = (req, res) => {
+const orderSchema = require("../../models/order");
+const User = require("../../models/user");
+const Product = require("../../models/product");
+const mongoose = require("mongoose")
 
-    if (!req.body) return res.status(400).send({ message: 'Client has not sent params' });
 
-    const order = orderSchema(req.body);
-    order.save()
-        .then((data) => {
-            res.json(data);
-        })
-        .catch((err) => {
-            res.json({ message: err });
+
+const createOrder = async (req, res) => {
+    const { userId, productId, cantidad } = req.body
+
+    if (!req.body) {
+        res.status(400).json({ message: "Cliente no ha enviado Parametros" });
+    }
+
+
+    if (mongoose.Types.ObjectId.isValid(userId) && mongoose.Types.ObjectId.isValid(productId)) {
+        const product = await Product.findOne({ _id: productId })
+        const user = await User.findOne({ _id: userId })
+        if (user === null && product === null) {
+            res.status(400).json({ message: "USER Y PRODUCT NO ENCONTRADOS" })
+        }
+
+        if (cantidad > product.inStock) {
+            res.status(409).json({ message: "No hay productos en stock" })
+        }
+        product.inStock = product.inStock - cantidad;
+
+        const order = await orderSchema({
+            ...req.body,
+            precio: product.price * cantidad
         });
-}
+        order.save()
+            .then((data) => {
+                Product.findByIdAndUpdate(
+                    product._id
+                    , product, function (err, result) {
+                        if (err) {
+                            res.status(409).json({ message: "error  update", err })
+                        }
+                        else {
+                            res.status(200).json({ message: "ok" })
+                        }
+
+                    });
+                res.json(data);
+            })
+            .catch((err) => {
+                res.json({ message: err });
+            });
+    } else {
+        res.status(400).json({ message: "no funciona" })
+    }
 
 
-module.exports = createOrder
+
+};
+
+module.exports = createOrder;
--- back/src/models/order.js
+++ back/src/models/order.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
 
     productId: { type: String, required: true },
-    userId: { type: String, require: true },
-    cantidad: { type: Number, require: true },
-    precio: { type: Number }
+    userId: { type: String, required: true },
+    cantidad: { type: Number, required: true },
+    precio: { type: Number, required: true }
 
 })
 module.exports = mongoose.model("order", orderSchema);
